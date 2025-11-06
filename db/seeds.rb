6c223d16ea1ae5a9570595be79284ad7626d260b
jsangalang2 <jsangalang2@academic.rrc.ca>
2025-11-06 10:08:46 -0600

    rails db:seed ran and successful

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,37 +13,37 @@ require 'faker'
 
 #reads all csv files
 
-product_csv = Rails.root.join('db/products.csv')
+# product_csv = Rails.root.join('db/products.csv')
 
-customer_csv = Rails.root.join('db/customers.csv')
+# customer_csv = Rails.root.join('db/customers.csv')
 
-#puts data into database
+# #puts data into database
 
-CSV.foreach(product_csv, headers: true) do |product|
-  puts "Seeding product: #{product.to_h}"
-    category = Category.find_or_create_by(name: product["category"])
-    Product.create!(
-      name: product["name"],
-      price: product["price"].to_f,
-      description: product["description"],
-      stock_quantity: product["stock quantity"].to_i,
-      category: category
-    )
+# CSV.foreach(product_csv, headers: true) do |product|
+#   puts "Seeding product: #{product.to_h}"
+#     category = Category.find_or_create_by(category: product["category"])
+#     Product.create!(
+#       name: product["name"],
+#       price: product["price"].to_f,
+#       description: product["description"],
+#       stock_quantity: product["stock quantity"].to_i,
+#       category: category
+#     )
 
-end
+# end
 
-puts "Seeding complete! Total Products: #{Product.count}"
+# puts "Seeding complete! Total Products: #{Product.count}"
 
-CSV.foreach(customer_csv, headers: true) do |customer|
-  puts "Seeding customer: #{customer.to_h}"
-  Customer.create!(
-    first_name: customer["first_name"],
-    last_name: customer["last_name"],
-    email: customer["email"]
-  )
-end
+# CSV.foreach(customer_csv, headers: true) do |customer|
+#   puts "Seeding customer: #{customer.to_h}"
+#   Customer.create!(
+#     first_name: customer["first_name"],
+#     last_name: customer["last_name"],
+#     email: customer["email"]
+#   )
+# end
 
-puts "Seeding complete! Total Customers: #{Customer.count}"
+# puts "Seeding complete! Total Customers: #{Customer.count}"
 
 # creating fake orders
 
@@ -68,7 +68,7 @@ orders.each do |order|
   rand(1..5).times do
     product = products.sample        # pick a random product
     quantity = rand(1..10)           # random quantity
-    price = product.price             # price from the product
+    price = quantity * product.price             # price from the product
 
     OrderItem.create!(
       order: order,
