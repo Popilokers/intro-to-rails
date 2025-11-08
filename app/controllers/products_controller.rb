eae446df6cfc14e5427b20d8c3aa5819f4af4e22
jsangalang2 <jsangalang2@academic.rrc.ca>
2025-11-08 00:01:17 -0600

    dropdown implemented

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,17 @@
 class ProductsController < ApplicationController
 def index
-  if params[:query].present?
-    @products = Product.where("name LIKE ?", "%#{params[:query]}%").order(:name)
-  else
-    @products = Product.all.order(:name)
-  end
+@products = Product.all
 
-  @products = @products.page(params[:page]).per(25).order(:name) # if using Kaminari
+# apply search filter
+@products = @products.where("name LIKE ?", "%#{params[:query]}%") if params[:query].present?
+
+# apply category filter
+@products = @products.where(category_id: params[:category_id]) if params[:category_id].present?
+
+# order and paginate
+@products = @products.order(:name).page(params[:page]).per(25)
+
+
 end
 
 
