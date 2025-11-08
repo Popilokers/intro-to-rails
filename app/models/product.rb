b851011c5088ff58e52adfbc74672e903a6fe50f
jsangalang2 <jsangalang2@academic.rrc.ca>
2025-11-08 00:29:08 -0600

    erb conditional implemented

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,5 @@ class Product < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: {greater_than: 0}
-  validates :description, presence: true
   validates :stock_quantity, presence: true, numericality:{only_integer: true, greater_than_or_equal_to: 0}
 end
